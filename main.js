de453b0f2e69e583b7bf8f8ba4a45fcee1b8a855
Joe Purdy <joe@poweredbypurdy.com>
2016-07-17 20:47:34 -0700

    first round of automated creep scaling

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-var roleHarvester = require('role.harvester');
-var roleUpgrader = require('role.upgrader');
-var roleBuilder = require('role.builder');
+var roleHarvester = require('role.harvester'),
+    roleUpgrader = require('role.upgrader'),
+    roleBuilder = require('role.builder');
+
+var MAX_HARVESTERS = 3,
+    MAX_UPGRADERS  = 2,
+    MAX_BUILDERS   = 2;
 
 module.exports.loop = function () {
 
@@ -14,23 +18,29 @@ module.exports.loop = function () {
 
     // Spawn Harvesters
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-    if(harvesters.length < 2) {
+    if(harvesters.length < MAX_HARVESTERS) {
         var newName = Game.spawns.Shadowhollow.createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
         console.log('Spawning new harvester: ' + newName);
+    } else if(harvesters.length === MAX_HARVESTERS && harvesters.length < 10) {
+        MAX_HARVESTERS += 1;
     }
 
     // Spawn Upgraders
     var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
-    if(upgraders.length < 1) {
+    if(upgraders.length < MAX_UPGRADERS) {
         var newName = Game.spawns.Shadowhollow.createCreep([WORK,CARRY,MOVE], undefined, {role: 'upgrader'});
         console.log('Spawning new upgrader: ' + newName);
+    } else if(upgraders.length === MAX_UPGRADERS && upgraders.length < 10) {
+        MAX_UPGRADERS += 1;
     }
 
     // Spawn Builders
     var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
-    if(builders.length < 1) {
+    if(builders.length < MAX_BUILDERS) {
         var newName = Game.spawns.Shadowhollow.createCreep([WORK,CARRY,MOVE], undefined, {role: 'builder'});
         console.log('Spawning new harvester: ' + newName);
+    } else if(builders.length === MAX_BUILDERS && builders.length < 10) {
+        MAX_BUILDERS += 1;
     }
 
     // Run creep roles
